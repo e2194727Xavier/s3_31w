07adc853d6fc5176030f90a72bd8a04ec09d0272
maryline888 <maryline888@gmail.com>
2023-01-31 16:38:31 -0500

    c1 premiere version du theme

---
+++ documents/README.md
@@ -0,0 +1,11 @@
+# Excercice #1 
+
+## Installation de WordPress et création de thèmes
+## Auteur : Maryline Carrier 
+### Objectifs 
+    - Installer WP 
+    - Créer un thème simple 
+    - Créer une structure Sass
+    - Créer un minimum de 10 commits 
+    - Déployer le site local vers un serveur distant
+
---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+// Enfiler la feuille de style
+function ajouter_styles()
+{
+    wp_enqueue_style(
+        '31w-style-principal', //id de la feuille de style
+        get_template_directory_uri() . '/style.css', //add url de la feuille de style
+        array(), //dependance avec les autres feuilles de style 
+        filemtime(get_template_directory() . '/style.css') // la version de la dernière feuille de style
+    );
+}
+add_action('wp_enqueue_scripts', 'ajouter_styles');

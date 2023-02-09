6db35d7ff7980de7f6db3a67a9b257c71c6114f8
maryline888 <maryline888@gmail.com>
2023-02-09 16:25:38 -0500

    ajout de la page erreur 404

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+
+/**modele footer.php ?> */ ?>
+<?php get_header(); ?>
+
+<main>
+    <code>404.php</code>
+
+    <h1>Erreur 404 -la page n'existe pas</h1>
+
+
+
+
+</main>
+<?php get_footer(); ?>

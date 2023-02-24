d0b723f110ca1015180ad3d6a78746c0b7e9f435
maryline888 <maryline888@gmail.com>
2023-02-23 20:27:29 -0500

    css erreur404

--- 404.php
+++ 404.php
@@ -5,6 +5,8 @@
 
 <main>
     <code>404.php</code>
-    <h1>Erreur 404 -la page n'existe pas</h1>
+    <section class="erreur">
+        <h1>Erreur 404 -la page n'existe pas</h1>
+    </section>
 </main>
 <?php get_footer(); ?>

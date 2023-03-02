debfb2f490e70e4ae5de28ea037c9ec25392db63
maryline888 <maryline888@gmail.com>
2023-03-02 15:21:56 -0500

    c10 intro aux templates-parts

---
+++ template-parts/categorie-notes-wp.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * templete-part qui permettra d'afficher un article provenant 
+ * d'un conteneur de classe blocFlex
+ * pour un article de categorie notes-wp
+ */
+$titre = get_the_title();
+// if (substr($titre, 0, 1)== "0"){} enlever le premier carac au cas ou ya un zero au debut 
+
+?>
+<article class="">
+    <h5><a href="<?php the_permalink(); ?>"> <?= $titre ?></a></h5>
+    <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+</article>
